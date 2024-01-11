76b9541bc367fca26715967b0d9385dce6b3bf34
Dani Bennet <danijulianbennet@gmail.com>
2024-01-11 07:49:05 +0530

    started work on MOve

--- Board/Move.java
+++ Board/Move.java
@@ -1,4 +1,42 @@
 package Pluton.Board;
 
-public class Move {
+import Pluton.pieces.Piece;
+
+public abstract class Move {
+
+    final Board board;
+    final Piece movedPiece;
+    final int destinationCoodrinate;
+
+
+    private Move(final Board board,
+         final Piece movedPiece,
+         final int destinationCoodrinate){
+        this.board = board;
+        this.movedPiece = movedPiece;
+        this.destinationCoodrinate = destinationCoodrinate;
+    }
+
+    public static final class MajorMove extends Move {
+
+        public MajorMove(final Board board,
+                  final Piece movedPiece,
+                  final int destinationCoodrinate) {
+            super(board, movedPiece, destinationCoodrinate);
+        }
+    }
+
+    public static final class AttackMove extends Move{
+
+        final Piece attackedPiece;
+        public AttackMove(final Board board,
+                   final Piece movedPiece,
+                   final int destinationCoodrinate,
+                   final Piece attackedPiece) {
+            super(board, movedPiece, destinationCoodrinate);
+            this.attackedPiece = attackedPiece;
+        }
+    }
+
+
 }
